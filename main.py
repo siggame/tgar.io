3c23d267e24c3354f963bfead7c44d7949c275e5
Brissette <adb5cw@rc05xcs213.managed.mst.edu>
2021-10-26 19:43:25 -0500

    initial commit

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-if __name__ == "__main__":
-  async def main():
-    while True:
-      print(await agetch())
+import websockets
+import asyncio
+import subprocess
+import os
 
-      loop.create_task(main())
-      loop.run_forever
+async def handler(ws, path):
+  print("connected")
+
+async def main():
+
+  # initialize the websockets server that receives IO from front end
+  async with websockets.serve(handler, host=os.environ["CLIENT_HOST"], port=os.environ["CLIENT_PORT_WS"]):
+    # initialize the server process that serves our front end
+    with subprocess.Popen(["python3", "serveclient.py"]):
+      # block this coroutine
+      await asyncio.Future()
+      
+
+loop = asyncio.get_event_loop()
+loop.create_task(main())
+loop.run_forever()
