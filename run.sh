a03f9a3e982f6fff8848160137bcd3769181809f
Kevin Lai <zlnh4@umsystem.edu>
2021-10-28 19:14:56 -0500

    run

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 export CLIENT_HOST="localhost"
 export CLIENT_PORT="3000"
 export CLIENT_PORT_WS="3001"
+
+poetry run tgar
--- tgar/main.py
+++ tgar/main.py
@@ -11,7 +11,7 @@ async def _main():
   # initialize the websockets server that receives IO from front end
   async with websockets.serve(handler, host=os.environ["CLIENT_HOST"], port=os.environ["CLIENT_PORT_WS"]):
     # initialize the server process that serves our front end
-    with subprocess.Popen(["python3", "serveclient.py"]):
+    with subprocess.Popen(["python3", "tgar/serveclient.py"]):
       # block this coroutine
       await asyncio.Future()
       
